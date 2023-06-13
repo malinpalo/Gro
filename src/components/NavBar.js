ec6ef4659c04dfcbb104b55c584e3dad929300bd
malinpalo <>
2023-06-13 12:09:51 +0000

    Update navbar with new icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,13 +14,13 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <Nav.Link>
-              <i className="fal fa-home"></i>Home
+              <i className="fas fa-home"></i>Home
             </Nav.Link>
             <Nav.Link>
-              <i className="fal fa-sign-in-alt"></i>Sign in
+              <i className="fas fa-sign-in-alt"></i>Sign in
             </Nav.Link>
             <Nav.Link>
-              <i className="fal fa-user-plus"></i>Sign up
+              <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
