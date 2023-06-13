01bd8383b70609e6d7b22314090e0fc5d219c653
malinpalo <>
2023-06-13 21:24:53 +0000

    Add new image to signup form

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -129,7 +129,7 @@ const SignUpForm = () => {
       >
         <Image
           className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero2.jpg"}
+          src={"https://res.cloudinary.com/dz0w8vqzx/image/upload/v1686691094/signup_gro_ngx6me.jpg"}
         />
       </Col>
     </Row>
