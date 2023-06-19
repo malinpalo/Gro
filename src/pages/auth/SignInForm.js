8d110d02be532e3a60e86631141cc657f4e8e550
malinpalo <malinpalo.mp@gmail.com>
2023-06-19 17:41:28 +0000

    Add sign in form and style

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -22,7 +22,32 @@ function SignInForm() {
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
+          <Form>	
+                <Form.Group controlId="username">	
+                    <Form.Label className="d-none">Username</Form.Label>	
+                    <Form.Control 	
+                        type="text" 	
+                        placeholder="Username"	
+                        name="username" 	
+                        className={styles.Input}	
+                    />	
+                </Form.Group>	
+                <Form.Group controlId="password">	
+                    <Form.Label className="d-none">Password</Form.Label>	
+                    <Form.Control 	
+                        type="password" 	
+                        placeholder="Password" 	
+                        name="password"	
+                        className={styles.Input}	
+                    />	
+                </Form.Group>	
+                <Button 	
+                    className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}	
+                    type="submit"	
+                >	
+                    Sign In	
+                </Button>	
+            </Form>
 
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
