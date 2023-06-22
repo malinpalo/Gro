2f3e08d704c14086d44b0243f3d2e58321938de8
malinpalo <malinpalo.mp@gmail.com>
2023-06-22 10:01:45 +0000

    Add the inifinte scroll logic on comment components

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,9 +9,12 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 import Comment from "../comments/Comment";
-
 import CommentCreateForm from "../comments/CommentCreateForm";
+
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -55,14 +58,20 @@ function PostPage() {
           "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment 
-              key={comment.id} 
-              {...comment} 
-              setPost={setPost}
-              setComments={setComments}
-              />
-            ))
+            <InfiniteScroll
+                children={comments.results.map((comment) => (
+                  <Comment 
+                    key={comment.id} 
+                    {...comment} 
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))}
+                dataLength={comments.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!comments.next}
+                next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments, don't be shy, be the first to add a comment!</span>
           ) : (
