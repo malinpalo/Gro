f2e38f3b9e171230e52d2c6c9d7ab5f08e229f1a
malinpalo <malinpalo.mp@gmail.com>
2023-06-25 17:41:00 +0000

    Add a dropdown menu for users so they can edit/update their own profile

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,10 +1,9 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import styles from "../styles/MoreDropdown.module.css";
+import { useHistory } from "react-router";
 
 
-// The forwardRef is important!! : ...
-// Dropdown needs access to the DOM node in order to position the Menu
 const ThreeDotsMenu = React.forwardRef(({ onClick }, ref) => (
     <i
       className="fas fa-ellipsis-v"
@@ -43,3 +42,34 @@ const ThreeDotsMenu = React.forwardRef(({ onClick }, ref) => (
         </Dropdown>
     );
   };
+
+  export function ProfileEditDropdown({ id }) {
+    const history = useHistory();
+    return (
+      <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+        <Dropdown.Toggle as={ThreeDotsMenu} />
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit`)}
+            aria-label="edit-profile"
+          >
+            <i className="far fa-edit" /> edit profile
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/username`)}
+            aria-label="edit-username"
+          >
+            <i className="fas fa-id-card" />
+            change username
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => history.push(`/profiles/${id}/edit/password`)}
+            aria-label="edit-password"
+          >
+            <i className="fas fa-key" />
+            change password
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
