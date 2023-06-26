a6880383896a3ee34de8832a557586839cd73b04
malinpalo <malinpalo.mp@gmail.com>
2023-06-26 20:49:25 +0000

    Optimize bootstrap imports and clean up in consloe.logs

--- src/pages/profiles/ProfileEditForm.js
+++ src/pages/profiles/ProfileEditForm.js
@@ -42,7 +42,7 @@ const ProfileEditForm = () => {
           const { name, description, image } = data;
           setProfileData({ name, description, image });
         } catch (err) {
-          console.log(err);
+          // console.log(err);
           history.push("/");
         }
       } else {
@@ -78,7 +78,7 @@ const ProfileEditForm = () => {
       }));
       history.goBack();
     } catch (err) {
-      console.log(err);
+      // console.log(err);
       setErrors(err.response?.data);
     }
   };
